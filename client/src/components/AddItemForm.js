0e367ebdedc4b9c40fc2d1644f03e242532f62da
Hilary Gould <gould.hilary.r@gmail.com>
2023-05-25 15:29:34 -0400

    Working on a form for adding items

---
+++ client/src/components/AddItemForm.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react"
+import translateServerErrors from "../services/translateServerErrors"
+import ErrorList from "./layout/ErrorList"
+
+const AddItemForm = props => {
+    const defaultList = {
+        name: ""
+    }
+}
