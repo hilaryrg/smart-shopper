2c1a711f9fa31318b45c52c1ba3e6bf0289a99bf
Hilary Gould <gould.hilary.r@gmail.com>
2023-05-31 21:17:15 -0400

    WIP - added styling to the home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,9 +2,18 @@ import React from "react"
 
 const Home = (props) => {
     return (
-        <div>
-            This is my Smart Shopper app
-            <img src="https://smart-shopper-production.s3.amazonaws.com/torbjorn-helgesen-C4FbCe4L_pw-unsplash.jpg"/>
+        <div className="home-page-image-and-text">
+            <div className="home-page-text-section">
+                <div className="home-page-text-title">
+                    Smart Shopper
+                </div>
+                <p className="home-page-text-body">
+                    The app that helps you create, manage, and organize your lists.
+                </p>
+            </div>
+            {/* <div className="home-page-image-container"> */}
+                <img className="home-page-image" src="https://smart-shopper-production.s3.amazonaws.com/torbjorn-helgesen-C4FbCe4L_pw-unsplash.jpg"/>
+            {/* </div> */}
         </div>
     )
 }
